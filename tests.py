9073263508afdcadb22875f84d2e711c86b192ed
Blake McCarn <bmccarn2@uncc.edu>
2022-12-06 13:11:39 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ class FlaskTest(unittest.TestCase):
         response = requests.get("http://127.0.0.1:5000/")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-        self.assertEqual('<h2>Use this site to maintain and organize your notes.</h2>' in response.text, True)
+        self.assertEqual('<h2>The Lightweight Task-Tracking Software!</h2>' in response.text, True)
 
     def test_notes(self):
         response = requests.get("http://127.0.0.1:5000/notes")
