be3ac5ceafe04a571e43eca928956f48f6427027
brettayre0 <112581283+brettayre0@users.noreply.github.com>
2022-12-11 08:48:39 -0500

    added tests for new features

--- tests.py
+++ tests.py
@@ -7,28 +7,43 @@ class FlaskTest(unittest.TestCase):
         response = requests.get("http://127.0.0.1:5000/")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-        self.assertEqual('<h2>The Lightweight Task-Tracking Software!</h2>' in response.text, True)
+        self.assertEqual('<h1>The Lightweight Task-Tracking Software!</h1>' in response.text, True)
 
     def test_notes(self):
         response = requests.get("http://127.0.0.1:5000/notes")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-       # self.assertEqual('Title' and 'Date' in response.text, True)
+        #self.assertEqual('Date' in response.text, True)
        
     def test_note(self):
-        response = requests.get("http://127.0.0.1:5000/notes/1")
+        response = requests.get("http://127.0.0.1:5000/notes/2")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-       # self.assertEqual('First Note' in response.text, True)
+        #self.assertEqual('Comment' in response.text, True)
 
     def test_new(self):
         response = requests.get("http://127.0.0.1:5000/notes/new")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-      # self.assertEqual('<form action="new" method="post">' in response.text, True)
+        #self.assertEqual('<form action="new" method="post">' in response.text, True)
 
     def test_delete(self):
         response = requests.get('http://127.0.0.1:5000/notes/delete')
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+    def test_users(self):
+        response = requests.get('http://127.0.0.1:5000/users')
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+    def test_profile(self):
+        response = requests.get('http://127.0.0.1:5000/profile')
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+    
+    def test_bugreport(self):
+        response = requests.get('http://127.0.0.1:5000/form')
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
         
